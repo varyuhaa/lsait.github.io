010b6eb4b006f3350b33eaf5b42db6606c2caa18
varyuhaa <63977174+varyuhaa@users.noreply.github.com>
2020-04-20 00:50:22 +0300

    new laba
    top

---
+++ knopka.js
@@ -0,0 +1,11 @@
+function createParagraph() {
+  let para = document.createElement('p');
+  para.textContent = 'Зачем нажал?!';
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', createParagraph); 
+}
